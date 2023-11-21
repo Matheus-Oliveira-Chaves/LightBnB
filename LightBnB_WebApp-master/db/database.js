7ecd47894957e0576116383276ed43eb2bfd7a82
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-11-20 23:43:24 -0500

    REFACTOR reservations to show real reservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -91,7 +91,30 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+    SELECT reservations.id,
+           properties.*,
+           reservations.start_date,
+           reservations.end_date,
+           AVG(property_reviews.rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+
+  return pool
+    .query(query, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /// Properties
