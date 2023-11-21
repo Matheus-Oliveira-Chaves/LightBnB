95bb783804b89e28a6c22f01bfbcd4d729d7fd76
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-11-20 23:30:49 -0500

    REFACTOR user email, id, adduser functions to work with sql

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -19,23 +19,43 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
-};
+  const query = `
+    SELECT *
+    FROM users
+    WHERE email = $1;
+  `;
 
+  return pool
+    .query(query, [email])
+    .then((result) => {
+      return result.rows[0] || null;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
+};
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const query = `
+    SELECT *
+    FROM users
+    WHERE id = $1;
+  `;
+
+  return pool
+    .query(query, [id])
+    .then((result) => {
+      return result.rows[0] || null;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /**
@@ -44,10 +64,23 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const query = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return pool
+    .query(query, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 /// Reservations
